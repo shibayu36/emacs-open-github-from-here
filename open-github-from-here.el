ea76a7d8088d4f8041cdb37228a6650f03ec96eb
shiba_yu36 <shibayu36@gmail.com>
2013-04-22 19:42:09 +0900

    browser command

--- open-github-from-here.el
+++ open-github-from-here.el
@@ -28,21 +28,26 @@
       (file-name-directory load-file-name)
     default-directory))
 
+(defvar open-github-from-here:browse-command "open")
+
 (defvar open-github-from-here:command
   (expand-file-name "open-github-from-file" open-github-from-here:command-dir))
 
 (defun open-github-from-here ()
   (interactive)
+  (message open-github-from-here:command)
   (cond ((and (open-github-from-here:git-project-p) (use-region-p))
          (shell-command
-          (format "%s %s %d %d"
+          (format "BROWSE_COMMAND=%s %s %s %d %d"
+                  open-github-from-here:browse-command
                   open-github-from-here:command
                   (file-name-nondirectory (buffer-file-name))
                   (line-number-at-pos (region-beginning))
                   (line-number-at-pos (region-end)))))
         ((open-github-from-here:git-project-p)
          (shell-command
-          (format "%s %s %d"
+          (format "BROWSE_COMMAND=%s %s %s %d"
+                  open-github-from-here:browse-command
                   open-github-from-here:command
                   (file-name-nondirectory (buffer-file-name))
                   (line-number-at-pos))))))
